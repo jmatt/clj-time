23c512a20f4f0bc7fe5825ae348cdbf816cbb5ba
Sam Ritchie <sritchie09@gmail.com>
2011-10-20 11:58:28 -0700

    added backtype prefix.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-time "0.3.2"
+(defproject backtype/clj-time "0.3.2"
   :description "A date and time library for Clojure, wrapping Joda Time."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [joda-time "1.6"]]
